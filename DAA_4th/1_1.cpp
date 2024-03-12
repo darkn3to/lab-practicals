d57c3c29a9cf873deeb4f8f6be3ad096dedb2d31
Dhruv <62509177+darkn3to@users.noreply.github.com>
2024-03-12 16:37:14 +0530

    function added

--- DAA_4th/1_1.cpp
+++ DAA_4th/1_1.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main() {
+void searchKeyInVector() {
     vector<int> v;
-    int x, key, t, comp=0, f=0;
+    int x, key, comp=0, f=0;
+    cout << "Enter the elements of the array: ";
+    cin >> x;
+    while (x!=-1) {
+        v.push_back(x);
+        cin >> x;
+    }
+    cout << "Enter the key: ";
+    cin >> key;
+    for (auto it:v) {
+        if (it==key) {
+            f=1; break;
+        }
+        ++comp;
+    }
+    if (f==1) cout << "Present " << comp << endl;
+    else cout << "Not Present " << endl;
+}
+int main() {
+    int t;
     cout << "Enter the number of test cases: ";
     cin >> t;
-    while (t-->0) {
-        cout << "Enter the elements of the array: ";
-        cin >> x;
-        while (x!=-1) {
-            v.push_back(x);
-            cin >> x;
-        }
-        cout << "Enter the key: ";
-        cin >> key;
-        for (auto it:v) {
-            if (it==key) {
-                f=1;
-                break;
-            }
-            ++comp;
-        }
-        if (f==1) cout << "Present " << comp << endl;
-        else cout << "Not Present " << endl;
-    }
+    while (t-->0) searchKeyInVector();
 }
