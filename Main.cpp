fb7ad7c98dd746e8fa7ce927e399a9b01d29c557
Manas12Singh <rd511030@gmail.com>
2024-03-12 09:52:09 +0530

    main added

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+//create functions here
+
+int main()
+{
+    int t;
+    cout << "Enter the number of test cases: ";
+    cin >> t;
+    while (t-- > 0)
+    {
+        // enter code here
+    }
+    return 0;
+}
