ca9ca6c752d3411af49e9136d0e1ff7a8b9661f3
Manas12Singh <rd511030@gmail.com>
2024-03-13 11:53:25 +0530

    done 2_3

--- DAA_4th/2_2.cpp
+++ DAA_4th/2_2.cpp
@@ -1,46 +1,34 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-bool searchTriplet(vector<int> &nums)
-{
+bool searchTriplet(vector<int> &nums) {
     int n = nums.size();
-    for (int k = n - 1; k > 1; k--)
-    {
+    for (int k = n - 1; k > 1; k--) {
         int i = 0, j = k - 1;
-        while (i < j)
-        {
+        while (i < j) {
             int sum = nums[i] + nums[j];
-            if (sum == nums[k])
-            {
+            if (sum == nums[k]) {
                 cout << i + 1 << ", " << j + 1 << ", " << k + 1 << endl;
                 return true;
             }
-            else if (sum < nums[k])
-                i++;
-            else
-                j--;
+            else if (sum < nums[k]) i++;
+            else j--;
         }
     }
     return false;
 }
-
-int main()
-{
+int main() {
     int t;
     cout << "Enter the number of test cases: ";
     cin >> t;
-    while (t-- > 0)
-    {
+    while (t-- > 0) {
         int n;
         cout << "Enter the number of elements: ";
         cin >> n;
         vector<int> nums(n);
         cout << "Enter the elements: ";
-        for (int i = 0; i < n; i++)
-            cin >> nums[i];
-        if (!searchTriplet(nums))
-            cout << "No sequence found." << endl;
+        for (int i = 0; i < n; i++) cin >> nums[i];
+        if (!searchTriplet(nums)) cout << "No sequence found." << endl;
         cout << endl;
     }
     return 0;
---
+++ DAA_4th/2_3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int countPair(vector<int> &nums, int target) {
+    sort(nums.begin(), nums.end());
+    int l = 0, r = 0, n = nums.size(), c = 0;
+    while (r < n) {
+        if (nums[r] - nums[l] == target) {
+            int i = 1, j = 1;
+            while (l < n - 1 && nums[l] == nums[l + 1]) l++, i++;
+            while (r < n - 1 && nums[r] == nums[r + 1]) r++, j++;
+            c += (i * j);
+            l++, r++;
+        }
+        else if (nums[r] - nums[l] < target) r++;
+        else l++;
+    }
+    return c;
+}
+int main() {
+    int t;
+    cout << "Enter the number of test cases: ";
+    cin >> t;
+    while (t-- > 0)
+    {
+        int n, target;
+        cout << "Enter the number of elements: ";
+        cin >> n;
+        vector<int> nums(n);
+        cout << "Enter the elements: ";
+        for (int i = 0; i < n; i++) cin >> nums[i];
+        cin >> target;
+        cout << countPair(nums, target) << endl;
+        cout << endl;
+    }
+    return 0;
+}
