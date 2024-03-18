c4e75fcd6b851a592b007227a600def1be14b84a
Manas12Singh <rd511030@gmail.com>
2024-03-18 10:32:02 +0530

    FIX

--- DAA_4th/4_1.cpp
+++ DAA_4th/4_1.cpp
@@ -1,61 +1,44 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
-
-void merges(vector<int> &nums, int l, int mid, int r, int &noc, int &noi)
-{
+void merges(vector<int> &nums, int l, int mid, int r, int &noc, int &noi) {
     int ls = mid - l + 1, rs = r - mid;
     vector<int> larr(ls), rarr(rs);
-    for (int i = 0; i < ls; i++)
-        larr[i] = nums[l + i];
-    for (int i = 0; i < rs; i++)
-        rarr[i] = nums[mid + i + 1];
+    for (int i = 0; i < ls; i++) larr[i] = nums[l + i];
+    for (int i = 0; i < rs; i++) rarr[i] = nums[mid + i + 1];
     int i = 0, j = 0, k = l;
-    while (i < ls && j < rs)
-    {
-        if (larr[i] <= rarr[j])
-            nums[k++] = larr[i++];
-        else
-        {
+    while (i < ls && j < rs) {
+        if (larr[i] <= rarr[j]) nums[k++] = larr[i++];
+        else {
             nums[k++] = rarr[j++];
             noi += mid - i + 1;
         }
         noc++;
     }
-    while (i < ls)
-        nums[k++] = larr[i++];
-    while (j < rs)
-        nums[k++] = rarr[j++];
+    while (i < ls) nums[k++] = larr[i++];
+    while (j < rs) nums[k++] = rarr[j++];
 }
-
-void mergeSort(vector<int> &nums, int l, int r, int &noc, int &noi)
-{
-    if (l < r)
-    {
+void mergeSort(vector<int> &nums, int l, int r, int &noc, int &noi) {
+    if (l < r) {
         int mid = (l + r) / 2;
         mergeSort(nums, l, mid, noc, noi);
         mergeSort(nums, mid + 1, r, noc, noi);
         merges(nums, l, mid, r, noc, noi);
     }
 }
-
-int main()
-{
+int main() {
     int t;
     cout << "Enter the number of test cases: ";
     cin >> t;
-    while (t-- > 0)
-    {
+    while (t-- > 0) {
         int n, noc = 0, noi = 0;
         cout << "Enter the number of elements: ";
         cin >> n;
         vector<int> nums(n);
         cout << "Enter the elements: ";
-        for (int i = 0; i < n; i++)
-            cin >> nums[i];
+        for (int i = 0; i < n; i++) cin >> nums[i];
         mergeSort(nums, 0, n - 1, noc, noi);
-        for (int i = 0; i < n; i++)
-            cout << nums[i] << " ";
+        for (int i = 0; i < n; i++) cout << nums[i] << " ";
         cout << endl;
         cout << "comparisons = " << noc << endl;
         cout << "inversions = " << noi << endl;
--- DAA_4th/4_2.cpp
+++ DAA_4th/4_2.cpp
@@ -1,55 +1,43 @@
 #include <iostream>
 #include <cstdlib>
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
-int part(vector<int> &nums, int l, int r, int &noc, int &nos)
-{
+int part(vector<int> &nums, int l, int r, int &noc, int &nos) {
     int pivot = l + rand() % (r - l + 1);
-    if (pivot != r)
-        swap(nums[pivot], nums[r]), nos++;
+    if (pivot != r) swap(nums[pivot], nums[r]), nos++;
     pivot = l - 1;
-    while (l < r)
-    {
+    while (l < r) {
         noc++;
-        if (nums[l] < nums[r])
-        {
+        if (nums[l] < nums[r]) {
             pivot++;
-            if (pivot != l)
-                swap(nums[pivot], nums[l]), nos++;
+            if (pivot != l) swap(nums[pivot], nums[l]), nos++;
         }
         l++;
     }
-    if (++pivot != r)
-        swap(nums[pivot], nums[r]), nos++;
+    if (++pivot != r) swap(nums[pivot], nums[r]), nos++;
     return pivot;
 }
-void randomQuickSort(vector<int> &nums, int l, int r, int &noc, int &nos)
-{
-    if (l < r)
-    {
+void randomQuickSort(vector<int> &nums, int l, int r, int &noc, int &nos) {
+    if (l < r) {
         int pivot = part(nums, l, r, noc, nos);
         randomQuickSort(nums, l, pivot - 1, noc, nos);
         randomQuickSort(nums, pivot + 1, r, noc, nos);
     }
 }
-int main()
-{
+int main() {
     srand((unsigned)time(nullptr));
     int t;
     cout << "Enter the number of test cases: ";
     cin >> t;
-    while (t-- > 0)
-    {
+    while (t-- > 0) {
         int n, noc = 0, nos = 0;
         cout << "Enter the number of elements: ";
         cin >> n;
         vector<int> nums(n);
         cout << "Enter the elements: ";
-        for (int i = 0; i < n; i++)
-            cin >> nums[i];
+        for (int i = 0; i < n; i++) cin >> nums[i];
         randomQuickSort(nums, 0, n - 1, noc, nos);
-        for (int i = 0; i < n; i++)
-            cout << nums[i] << " ";
+        for (int i = 0; i < n; i++) cout << nums[i] << " ";
         cout << endl;
         cout << "comparisons = " << noc << endl;
         cout << "swaps = " << nos << endl;
